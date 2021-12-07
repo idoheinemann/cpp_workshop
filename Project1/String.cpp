bb9df5c42527faaa48928dd0d8127da40f1a72c2
idoheinemann <idohaineman@gmail.com>
2021-12-07 17:56:15 +0200

    documented & added features

--- Project1/String.cpp
+++ Project1/String.cpp
@@ -1,16 +1,24 @@
+#include <string.h>
 #include "String.h"
 
-int c_str_len(const char* c_str)
-{
-	int s = 0;
-	while (c_str[s] != 0) {
-		s++;
+int count_matches(const char* c_str, const char* search) {
+	int count = 0;
+	int search_len = strlen(search);
+	int c_len = strlen(c_str);
+	for (int i = 0; i < c_len - search_len;) {
+		if (strncmp(c_str + i, search, search_len) == 0) {
+			count++;
+			i += search_len; // skip over all this match
+		}
+		else {
+			i++;
+		}
 	}
-	return s;
+	return count;
 }
 
 const char* c_str_copy(const char* c_str) {
-	int len = c_str_len(c_str);
+	int len = strlen(c_str);
 	char* new_char = new char[len + 1];
 	for (int i = 0; i < len; i++) {
 		new_char[i] = c_str[i];
@@ -22,7 +30,11 @@ const char* c_str_copy(const char* c_str) {
 String::String(const char* c_str)
 {
 	this->m_c_str = c_str_copy(c_str);
-	this->m_size = c_str_len(this->m_c_str);
+	this->m_size = strlen(this->m_c_str);
+}
+
+String::String(const String& other) : String(other.m_c_str)
+{
 }
 
 String::String() : String("")
@@ -51,7 +63,7 @@ String String::operator+(String const& other) const
 		new_c_str[i + len1] = other.m_c_str[i];
 	}
 	new_c_str[len1 + len2] = 0; // null term
-	return String(new_c_str);
+	return String(new_c_str, len1 + len2);
 	
 }
 
@@ -71,10 +83,67 @@ String::~String()
 	delete[] this->m_c_str;
 }
 
-String String::unsafe(const char* c_str)
+String& String::operator=(String const& other)
 {
-	String s;
-	s.m_c_str = c_str;
-	s.m_size = c_str_len(c_str);
-	return s;
+	if (this != &other) {
+		this->m_c_str = c_str_copy(other.m_c_str);
+		this->m_size = other.m_size;
+	}
+	return *this;
 }
+
+String String::replace(String substring, String newstring)
+{
+	int size = (newstring.m_size - substring.m_size) * count_matches(this->m_c_str, substring.m_c_str);
+	size += this->m_size;
+	char* new_str = new char[size + 1];
+	for (int i = 0, o = 0; i < size;) {
+		if (strncmp(this->m_c_str + o, substring.m_c_str, substring.m_size) == 0) {
+			for (int j = 0; j < newstring.m_size; j++) {
+				new_str[i + j] = newstring.m_c_str[j];
+			}
+			i += newstring.m_size;
+			o += substring.m_size;
+		}
+		else {
+			new_str[i++] = this->m_c_str[o++];
+		}
+	}
+	new_str[size] = 0;
+	return String(new_str, size);
+}
+
+String String::insert(String substring, int offset)
+{
+	if (offset > this->m_size) {
+		throw StringIndexException("offset larger then string size");
+	}
+	if (offset < 0) {
+		throw StringIndexException("offset negative");
+	}
+	int size = substring.m_size;
+	size += this->m_size;
+	char* new_str = new char[size + 1];
+	int i = 0, o = 0, c = 0;
+	for (;i < offset;) {
+		new_str[i++] = this->m_c_str[o++];
+	}
+	for (; c < substring.m_size && i < size;) {
+		// to me it is obvious that i < size if always true
+		// to the compiler, not so much
+		new_str[i++] = substring.m_c_str[c++];
+	}
+	for (; i < size;) {
+		new_str[i++] = this->m_c_str[o++];
+	}
+	new_str[size] = 0;
+	return String(new_str, size);
+}
+
+
+String::String(const char* c_str, int size)
+{
+	this->m_c_str = c_str;
+	this->m_size = size;
+}
+
--- Project1/main.cpp
+++ Project1/main.cpp
@@ -10,4 +10,14 @@ int main() {
     cout << s1.c_str() << endl;
     cout << s2.c_str() << endl;
     cout << (s1 + s2).c_str() << endl;
+    String s3 = s2;
+    s3.~String(); // should not delete s2, copy constructor
+    cout << s2.c_str() << endl;
+    s3 = s1;
+    s3.~String(); // should not delete s1, assignment overload
+    cout << s1.c_str() << endl;
+    s3 = s1.replace(String("l"), String("go"));
+    cout << s3.c_str() << endl;
+    s3 = s2.insert(String("ww."), 2);
+    cout << s3.c_str() << endl;
 }
