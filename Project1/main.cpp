ad02b0d3aade01900a530a7f50c23f73cce9269d
idoheinemann <idohaineman@gmail.com>
2021-12-07 11:05:52 +0200

    changed to setw instead of resize

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -1,18 +1,17 @@
 
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <iomanip>
 
 using namespace std;
 
 #define STR_SIZE 3
 
 int main() {
-    string str;
     for (int i = 1; i < 12; i++) {
         for (int j = 1; j < 12; j++) {
-            str = to_string(i * j);
-            str.resize(STR_SIZE, ' ');
-            cout << str << "|";
+            cout << setw(STR_SIZE) << i * j << "|";
         }
         cout << endl;
     }
